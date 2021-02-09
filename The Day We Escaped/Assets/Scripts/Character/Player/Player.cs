52aa66a9dfaf76f5691ced47a3c954af60c9932e
Nils Meijer <meijernils9@gmail.com>
2021-02-09 12:22:29 +0100

    Created some system foundation scripts, working on basic player movement

---
+++ The Day We Escaped/Assets/Scripts/Character/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerMovement _movementModule;
+
+    private void Awake()
+    {
+        _movementModule = GetComponent<PlayerMovement>();
+    }
+}
---
+++ The Day We Escaped/Assets/Scripts/Character/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [Range(0.5f, 10)] [SerializeField] private float _moveSpeed = 3f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+
+    private void FixedUpdate()
+    {
+        checkMovement();
+        checkJump();
+        checkCrouch();
+    }
+
+    private void checkMovement()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal") * _moveSpeed * Time.fixedDeltaTime;
+        float verticalAxis = Input.GetAxis("Vertical") * _moveSpeed * Time.fixedDeltaTime;
+
+        transform.position = new Vector3(transform.position.x + horizontalAxis, transform.position.y, transform.position.z + verticalAxis);
+
+        rb.MovePosition(transform.position.normalized);
+    }
+
+    private void checkJump()
+    {
+
+    }
+
+    private void checkCrouch()
+    {
+
+    }
+}
