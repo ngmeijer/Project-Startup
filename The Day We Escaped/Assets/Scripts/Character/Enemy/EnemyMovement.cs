4fae250a649b29f3cefaa411dbe0afe41d5104ad
Nils Meijer <meijernils9@gmail.com>
2021-02-11 23:02:35 +0100

    Working on enemy AI movement

--- The Day We Escaped/Assets/Scripts/Character/Enemy/EnemyMovement.cs
+++ The Day We Escaped/Assets/Scripts/Character/Enemy/EnemyMovement.cs
@@ -1,18 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private EnemyAlertLevel alertLevel;
+    private NavMeshAgent agent;
+    private Transform currentTarget;
+    private bool arrivedAtTarget;
+
+    [SerializeField] private float _walkRange = 10f;
+    [SerializeField] private List<Transform> _patrolPoints = new List<Transform>();
+
+    private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        if (agent == null)
+        {
+            this.gameObject.AddComponent<NavMeshAgent>();
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        setNewDestination();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Choose random point from list of patrolPoints. Then based on which one was chosen, choose a new one after arriving on point, or after certain duration.
+
+        if (arrivedAtTarget)
+        {
+            setNewDestination();
+        }
+
+        StartCoroutine(checkIfArrived());
+    }
+
+    private void setNewDestination()
+    {
+        arrivedAtTarget = false;
+
+        Transform newPoint = findNewPoint();
+
+        agent.SetDestination(newPoint.position);
+    }
+
+    private Transform findNewPoint()
+    {
+        int index = Random.Range(0, _patrolPoints.Count);
+
+        Transform newTarget = _patrolPoints[index];
+        currentTarget = newTarget;
+
+        return newTarget;
+    }
+
+    private IEnumerator checkIfArrived()
+    {
+        while (transform.position != currentTarget.position)
+        {
+            Debug.Log("not yet at target");
+            yield return null;
+        }
+
+        Debug.Log("Arrived at target.");
+        arrivedAtTarget = true;
+
+        yield return null;
     }
 }
--- The Day We Escaped/Assets/Scripts/SecuritySystem/FOVAdvanced.cs
+++ The Day We Escaped/Assets/Scripts/SecuritySystem/FOVAdvanced.cs
@@ -1,165 +1,254 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+enum EnemyAlertLevel
+{
+    Idle,
+    Suspicious,
+    Aware,
+};
 
 public class FOVAdvanced : MonoBehaviour
 {
-    private enum FOVDirection
+    private EnemyMovement _movement;
+    private EnemyAlertLevel _alertLevel;
+    private Camera _camera;
+    private List<Collider> _playersFound = new List<Collider>();
+    private Light _light;
+    private bool _foundCollider;
+
+    private float _timer;
+
+    [Header("Security settings")]
+    [SerializeField] [Range(0, 5)] private float _dangerDelay;
+
+    [Header("Light settings")]
+    [SerializeField] private float _range;
+    [SerializeField] private float _intensity;
+
+    private void Awake()
     {
-        Horizontal,
-        Vertical,
-    };
+        _camera = GetComponent<Camera>();
+        if (_camera == null)
+            this.gameObject.AddComponent<Camera>();
 
-    [SerializeField] private FOVDirection FOVDir;
-
-    [SerializeField] [Range(1, 50)] private int _amountOfRaysHorizontal;
-    [SerializeField] [Range(2, 25)] private int _amountOfRaysVertical;
-
-    private int usedAmountOfRays;
-
-    [SerializeField] [Range(1, 20)] private float _viewZRange;
-    [SerializeField] [Range(1, 100)] private float _fov;
-
-    [SerializeField] [Range(-0.05f, 0.05f)] private float _scanSpeed;
-
-    [SerializeField] private Mesh mesh;
-
-    [SerializeField] private LayerMask _targetMask;
-    [SerializeField] private LayerMask _obstacleMask;
-
-    [SerializeField] private Camera cam;
-
-    private float distanceBetweenRaysX;
-    private float distanceBetweenRaysY;
-
-    private float oldPosX;
-    private float oldPosY;
-
-    private Rect frustumSize;
-    private List<Vector3> directionsList = new List<Vector3>();
-
-    private void Start()
-    {
-        cam.farClipPlane = _viewZRange;
-        cam.fieldOfView = _fov;
-
-        float frustumHeight = 1.0f * _viewZRange * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
-        float frustumWidth = frustumHeight * cam.aspect;
-
-        //Y-axis, TOP & BOTTOM
-        frustumSize.yMin = frustumHeight;
-        frustumSize.yMax = -frustumHeight;
-
-        //X-axis LEFT & RIGHT
-        frustumSize.xMin = -frustumWidth;
-        frustumSize.xMax = frustumWidth;
-
-        switch (FOVDir)
+        _light = GetComponentInChildren<Light>();
+        if (_light == null)
         {
-            case FOVDirection.Horizontal:
-                oldPosX = -frustumWidth;
-                distanceBetweenRaysX = (frustumWidth * 2) / _amountOfRaysHorizontal;
-
-                for (int x = 0; x < _amountOfRaysHorizontal; x++)
-                {
-                    Vector3 directionVec = new Vector3(oldPosX, oldPosY, _viewZRange);
-                }
-                break;
-            case FOVDirection.Vertical:
-                break;
+            Transform child = this.gameObject.transform.GetChild(0);
+            child.gameObject.AddComponent<Light>();
+            _light = GetComponentInChildren<Light>();
         }
     }
 
-    //Only for testing & scene view. 
-    private void OnDrawGizmos()
+    private void Start()
     {
-        Gizmos.matrix = transform.localToWorldMatrix;
+        _light.range = _range;
 
-        cam.farClipPlane = _viewZRange;
-        cam.fieldOfView = _fov;
-
-        float frustumHeight = 1.0f * _viewZRange * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
-        float frustumWidth = frustumHeight * cam.aspect;
-
-        //Y-axis, TOP & BOTTOM
-        frustumSize.yMin = frustumHeight;
-        frustumSize.yMax = -frustumHeight;
-
-        //X-axis LEFT & RIGHT
-        frustumSize.xMin = -frustumWidth;
-        frustumSize.xMax = frustumWidth;
-
-        Gizmos.color = Color.red;
-
-        switch (FOVDir)
-        {
-            case FOVDirection.Horizontal:
-                oldPosX = -frustumWidth;
-                distanceBetweenRaysX = (frustumWidth * 2) / _amountOfRaysHorizontal;
-
-                oldPosY -= _scanSpeed;
-
-                for (int x = 0; x < _amountOfRaysHorizontal; x++)
-                {
-                    oldPosX += distanceBetweenRaysX;
-                    Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, oldPosY, _viewZRange));
-                }
-
-                if (oldPosY <= frustumSize.yMax || oldPosY >= frustumSize.yMin)
-                    _scanSpeed *= -1;
-
-                Gizmos.DrawLine(Vector3.zero, new Vector3(-frustumWidth, oldPosY, _viewZRange));
-                Gizmos.DrawLine(Vector3.zero, new Vector3(frustumWidth, oldPosY, _viewZRange));
-                break;
-
-            case FOVDirection.Vertical:
-                oldPosY = -frustumHeight * 2;
-
-                distanceBetweenRaysY = (frustumHeight * 4) / _amountOfRaysVertical;
-
-                oldPosX -= _scanSpeed;
-
-                for (int y = 0; y < _amountOfRaysVertical; y++)
-                {
-                    oldPosY += distanceBetweenRaysY;
-                    Vector3 direction = new Vector3(oldPosY, oldPosY, _viewZRange);
-                    Gizmos.DrawLine(Vector3.zero, direction);
-                    directionsList.Add(direction);
-                }
-
-
-                if (oldPosX <= frustumSize.xMin || oldPosX >= frustumSize.xMax)
-                    _scanSpeed *= -1;
-
-                Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, frustumHeight * 2, _viewZRange));
-                Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, -frustumHeight * 2, _viewZRange));
-
-                break;
-        }
-
+        findTargetsInScene();
     }
 
     private void Update()
     {
-        findVisibleTargets();
+        handleAlertState();
     }
 
-    private void findVisibleTargets()
+    private void findTargetsInScene()
     {
-        Debug.DrawRay(transform.position, transform.forward, Color.yellow);
-        Debug.Log(directionsList.Count);
-        foreach (Vector3 directionVec in directionsList)
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
         {
-            RaycastHit hit;
-            Ray directionRay = new Ray(transform.position, directionVec);
-            if (Physics.Raycast(directionRay, out hit, _viewZRange))
-            {
-                if (hit.collider.CompareTag("Player"))
-                {
-                    Debug.Log("hit player");
-                }
-            }
+            Collider col = player.GetComponent<Collider>();
+            _playersFound.Add(col);
         }
     }
+
+    private bool isTargetInView()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_camera);
+
+        foreach (Collider playercCollider in _playersFound)
+        {
+            if (GeometryUtility.TestPlanesAABB(planes, playercCollider.bounds))
+                _foundCollider = true;
+            else
+                _foundCollider = false;
+        }
+
+        return _foundCollider;
+    }
+
+    private void handleAlertState()
+    {
+        if (isTargetInView() == true)
+        {
+            _timer += Time.deltaTime;
+            if (_timer < _dangerDelay)
+            {
+                _alertLevel = EnemyAlertLevel.Suspicious;
+                _light.color = Color.yellow;
+            }
+
+            if (_timer > _dangerDelay)
+            {
+                _alertLevel = EnemyAlertLevel.Aware;
+                _light.color = Color.red;
+            }
+        }
+        else
+        {
+            _light.color = Color.white;
+            _alertLevel = EnemyAlertLevel.Idle;
+            _light.color = Color.white;
+            _timer = 0;
+        }
+    }
+
+    #region 
+
+    //private enum FOVDirection
+    //{
+    //    Horizontal,
+    //    Vertical,
+    //};
+
+    //[SerializeField] private FOVDirection FOVDir;
+
+    //[SerializeField] [Range(1, 50)] private int _amountOfRaysHorizontal;
+    //[SerializeField] [Range(2, 25)] private int _amountOfRaysVertical;
+
+    //private int usedAmountOfRays;
+
+    //[SerializeField] [Range(1, 20)] private float _viewZRange;
+    //[SerializeField] [Range(1, 100)] private float _fov;
+
+    //[SerializeField] [Range(-0.05f, 0.05f)] private float _scanSpeed;
+
+    //[SerializeField] private Mesh mesh;
+
+    //[SerializeField] private LayerMask _targetMask;
+    //[SerializeField] private LayerMask _obstacleMask;
+
+    //[SerializeField] private Camera cam;
+
+    //private float distanceBetweenRaysX;
+    //private float distanceBetweenRaysY;
+
+    //private float oldPosX;
+    //private float oldPosY;
+
+    //private Rect frustumSize;
+    //[SerializeField] private List<Vector3> directionsList = new List<Vector3>();
+    //public Vector3 direction = new Vector3();
+    //private float oldRotationY;
+
+    //private void Start()
+    //{
+    //    Vector3 topLeftFrustumPos = new Vector3();
+    //}
+
+    ////Only for testing & scene view. 
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.matrix = transform.localToWorldMatrix;
+
+    //    cam.farClipPlane = _viewZRange;
+    //    cam.fieldOfView = _fov;
+
+    //    float frustumHeight = 1.0f * _viewZRange * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+    //    float frustumWidth = frustumHeight * cam.aspect;
+
+    //    //Y-axis, TOP & BOTTOM
+    //    frustumSize.yMin = frustumHeight;
+    //    frustumSize.yMax = -frustumHeight;
+
+    //    //X-axis LEFT & RIGHT
+    //    frustumSize.xMin = -frustumWidth;
+    //    frustumSize.xMax = frustumWidth;
+
+    //    Gizmos.color = Color.red;
+
+    //    switch (FOVDir)
+    //    {
+    //        case FOVDirection.Horizontal:
+    //            oldPosX = -frustumWidth;
+    //            distanceBetweenRaysX = (frustumWidth * 2) / _amountOfRaysHorizontal;
+
+    //            oldPosY -= _scanSpeed;
+
+    //            for (int x = 0; x < _amountOfRaysHorizontal; x++)
+    //            {
+    //                oldPosX += distanceBetweenRaysX;
+    //                Vector3 directionVector3 = new Vector3(oldPosX, oldPosY, _viewZRange);
+    //                Gizmos.DrawLine(Vector3.zero, directionVector3);
+    //            }
+
+    //            if (oldPosY <= frustumSize.yMax || oldPosY >= frustumSize.yMin)
+    //                _scanSpeed *= -1;
+
+    //            Gizmos.DrawLine(Vector3.zero, new Vector3(-frustumWidth, oldPosY, _viewZRange));
+    //            Gizmos.DrawLine(Vector3.zero, new Vector3(frustumWidth, oldPosY, _viewZRange));
+    //            break;
+
+    //        case FOVDirection.Vertical:
+    //            oldPosY = -frustumHeight * 2;
+
+    //            distanceBetweenRaysY = (frustumHeight * 4) / _amountOfRaysVertical;
+
+    //            oldPosX -= _scanSpeed;
+
+    //            for (int y = 0; y < _amountOfRaysVertical; y++)
+    //            {
+    //                oldPosY += distanceBetweenRaysY;
+    //                Vector3 direction = new Vector3(oldPosY, oldPosY, _viewZRange);
+    //                Gizmos.DrawLine(Vector3.zero, direction);
+    //            }
+
+
+    //            if (oldPosX <= frustumSize.xMin || oldPosX >= frustumSize.xMax)
+    //                _scanSpeed *= -1;
+
+    //            Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, frustumHeight * 2, _viewZRange));
+    //            Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, -frustumHeight * 2, _viewZRange));
+    //            break;
+    //    }
+    //}
+
+    //private void Update()
+    //{
+    //    findVisibleTargets();
+    //}
+
+    //private void findVisibleTargets()
+    //{
+    //    float newRotationY = transform.rotation.y * Mathf.Rad2Deg;
+
+    //    float degreesMoved = newRotationY - oldRotationY;
+
+    //    float yPos = frustumSize.yMin;
+
+    //    Vector3 currentRotation = rotateAroundPoint(transform.position, direction, degreesMoved);
+
+    //    Debug.DrawRay(transform.position,  new Vector3(transform.localRotation.x, transform.localRotation.y, transform.localRotation.z) * 3, Color.cyan);
+
+    //    oldRotationY = transform.rotation.y * Mathf.Rad2Deg;
+
+    //    ////Find local position of a point on the frustum to find the correct position for a ray/line!
+    //    //Debug.DrawLine(transform.position, transform.localPosition + directionsList[0], Color.green);
+    //}
+
+    //private Vector3 rotateAroundPoint(Vector3 sun, Vector3 planet, float angle)
+    //{
+    //    angle *= Mathf.Deg2Rad;
+
+    //    float x = Mathf.Cos(angle) * (sun.x - planet.x) - Mathf.Sin(angle) * (sun.y - planet.y) + planet.x;
+    //    float y = Mathf.Sin(angle) * (sun.x - planet.x) - Mathf.Cos(angle) * (sun.y - planet.y) + planet.y;
+
+    //    return new Vector3(x, y);
+    //}
+
+    #endregion
 }
