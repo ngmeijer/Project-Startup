79c6435ee98a526bb6667bdd90de600882c75960
Nils Meijer <meijernils9@gmail.com>
2021-02-11 13:25:30 +0100

    Working on raycasts

--- The Day We Escaped/Assets/FOVAdvanced.cs
+++ The Day We Escaped/Assets/FOVAdvanced.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,8 +16,13 @@ public class FOVAdvanced : MonoBehaviour
     [SerializeField] [Range(1, 50)] private int _amountOfRaysHorizontal;
     [SerializeField] [Range(2, 25)] private int _amountOfRaysVertical;
 
-    [SerializeField] private float _viewZRange;
-    [SerializeField] private float _maxHeight;
+    private int usedAmountOfRays;
+
+    [SerializeField] [Range(1, 20)] private float _viewZRange;
+    [SerializeField] [Range(1, 100)] private float _fov;
+
+    [SerializeField] [Range(-0.05f, 0.05f)] private float _scanSpeed;
+
 
     [SerializeField] private Mesh mesh;
 
@@ -25,32 +31,47 @@ public class FOVAdvanced : MonoBehaviour
 
     [SerializeField] private Camera cam;
 
-    public float distanceBetweenRaysX;
-    public float distanceBetweenRaysY;
+    private float distanceBetweenRaysX;
+    private float distanceBetweenRaysY;
 
     private float oldPosX;
     private float oldPosY;
 
-    private float frustumBottom;
-    private float frustumTop;
-    private float frustumLeft;
-    private float frustumRight;
+    private Rect frustumSize;
+    private List<Vector3> directionsList = new List<Vector3>();
 
-    [SerializeField] private float _scanSpeed = 0.01f;
+    private void Start()
+    {
+        switch (FOVDir)
+        {
+            case FOVDirection.Horizontal:
+                for (int x = 0; x < _amountOfRaysHorizontal; x++)
+                {
+
+                }
+                break;
+            case FOVDirection.Vertical:
+                break;
+        }
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.matrix = transform.localToWorldMatrix;
 
         cam.farClipPlane = _viewZRange;
+        cam.fieldOfView = _fov;
 
         float frustumHeight = 1.0f * _viewZRange * Mathf.Tan(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
         float frustumWidth = frustumHeight * cam.aspect;
 
-        frustumTop = frustumHeight;
-        frustumBottom = -frustumHeight;
-        frustumLeft = -frustumWidth;
-        frustumRight = frustumWidth;
+        //Y-axis, TOP & BOTTOM
+        frustumSize.yMin = frustumHeight;
+        frustumSize.yMax = -frustumHeight;
+
+        //X-axis LEFT & RIGHT
+        frustumSize.xMin = -frustumWidth;
+        frustumSize.xMax = frustumWidth;
 
         Gizmos.color = Color.red;
 
@@ -68,7 +89,7 @@ public class FOVAdvanced : MonoBehaviour
                     Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, oldPosY, _viewZRange));
                 }
 
-                if (oldPosY <= frustumBottom || oldPosY >= frustumTop)
+                if (oldPosY <= frustumSize.yMax || oldPosY >= frustumSize.yMin)
                     _scanSpeed *= -1;
 
                 Gizmos.DrawLine(Vector3.zero, new Vector3(-frustumWidth, oldPosY, _viewZRange));
@@ -85,11 +106,13 @@ public class FOVAdvanced : MonoBehaviour
                 for (int y = 0; y < _amountOfRaysVertical; y++)
                 {
                     oldPosY += distanceBetweenRaysY;
-                    Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, oldPosY, _viewZRange));
+                    Vector3 direction = new Vector3(oldPosY, oldPosY, _viewZRange);
+                    Gizmos.DrawLine(Vector3.zero, direction);
+                    directionsList.Add(direction);
                 }
 
 
-                if (oldPosX <= frustumLeft || oldPosX >= frustumRight)
+                if (oldPosX <= frustumSize.xMin || oldPosX >= frustumSize.xMax)
                     _scanSpeed *= -1;
 
                 Gizmos.DrawLine(Vector3.zero, new Vector3(oldPosX, frustumHeight * 2, _viewZRange));
@@ -98,5 +121,28 @@ public class FOVAdvanced : MonoBehaviour
                 break;
         }
 
+    }
+
+    private void Update()
+    {
+        findVisibleTargets();
+    }
+
+    private void findVisibleTargets()
+    {
+        Debug.DrawRay(transform.position, transform.forward, Color.yellow);
+        Debug.Log(directionsList.Count);
+        foreach (Vector3 directionVec in directionsList)
+        {
+            RaycastHit hit;
+            Ray directionRay = new Ray(transform.position, directionVec);
+            if (Physics.Raycast(directionRay, out hit, _viewZRange))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("hit player");
+                }
+            }
+        }
     }
 }
